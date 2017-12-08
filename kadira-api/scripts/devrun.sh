9c8b975b639722b70fbfb37166e032072cec7b5f
ivan046 <windmillvan@gmail.com>
2017-12-08 12:45:35 +0300

    kadiraDB, alertsman fix, mailgun creds cleaning

--- kadira-api/scripts/devrun.sh
+++ kadira-api/scripts/devrun.sh
@@ -2,7 +2,7 @@
 
 MONGO_APP_URL=mongodb://localhost/apm \
 MONGO_SHARD_URL_one=mongodb://localhost/apm \
-MAIL_URL=smtp://postmaster%40arunoda.mailgun.org:71d5vp8eji62@smtp.mailgun.org:587 \
+MAIL_URL=smtp://user:pass@smtp.mailgun.org:587 \
 JWT_SECRET="secret" \
 JWT_LIFETIME="1d" \
 AUTH_SECRET="secret" \
